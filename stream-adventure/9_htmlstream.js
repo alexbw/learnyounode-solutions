1ffa7da4976dd1068972ff4171685d2cb93aba6c
Alex Wiltschko <alex.bw@gmail.com>
2014-03-08 17:26:46 -0500

    Moving onwards

--- stream-adventure/9_htmlstream.js
+++ stream-adventure/9_htmlstream.js
@@ -1,3 +1,15 @@
 var trumpet = require('trumpet')
+var tr = trumpet()
 var through = require('through')
 
+var ts = through( function write (data) {
+    this.emit(data.toString().toUpperCase())
+})
+
+process.stdin.pipe(tr)
+var stream = tr.select("loud").createStream()
+
+process.stdin
+    .pipe(stream)
+    .pipe(ts)
+    .pipe(process.stdout)
